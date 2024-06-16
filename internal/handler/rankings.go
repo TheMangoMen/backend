07385f364c3563e1816f9ad44313e1159f487a3f
Norman Chen <njchen@uwaterloo.ca>
2024-06-16 12:05:35 -0400

    add both production and local

--- internal/handler/rankings.go
+++ internal/handler/rankings.go
@@ -13,7 +13,7 @@ import (
 
 func GetRankings(rs service.RankingService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		jIDStr := r.PathValue("jID")
+		jIDStr := r.PathValue("jid")
 		jID, err := strconv.Atoi(jIDStr)
 		if err != nil {
 			http.Error(w, "invalid job id", http.StatusBadRequest)
