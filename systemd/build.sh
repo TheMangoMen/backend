e53d702445b98771d3f8d80be764239873306a2b
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2024-07-06 15:35:30 -0400

    return exit code + full go path

--- systemd/build.sh
+++ systemd/build.sh
@@ -3,4 +3,5 @@
 git -C /home/debian/backend reset HEAD --hard
 git -C /home/debian/backend pull --rebase
 
-go -C /home/debian/backend build cmd/app/main.go
+/usr/local/go/bin/go -C /home/debian/backend mod tidy
+/usr/local/go/bin/go -C /home/debian/backend build cmd/app/main.go
